34d8cbe86cb9f488ccc011c150995db79ddf8d69
ishan10 <ikpandya10@gmail.com>
2018-05-20 15:41:28 -0700

    Webpack Configuration : Handling JS,JSX and output to file with unique hash

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,30 @@ const __PROD__ = 'production';
 const config ={
 	entry : './src/index.js',
 	output : {
-		filename : 'bundle.js',
+		filename : '[name].[chunkhash].js',
 		path : path.resolve(__dirname, './dist/')
 	},
 	devServer : {
 		contentBase : path.resolve(__dirname, './src/'),
 		port:4000
 	},
+	module :{
+		rules :[
+			{
+				/**
+				* Javascript
+				**/
+				test:/\.js|jsx$/,
+				exclude : /(node_modules)/,
+				use : {
+				loader : 'babel-loader',
+				options : {
+					presets:['es2015', 'stage-0', 'react']
+					}
+				}
+			},
+			]
+		},
 	mode : __DEV__
 }
   
